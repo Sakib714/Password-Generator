c7256a2a691d4199e716f9fced0f8b6d9556d44c
Sadman Sakib <78845308+Sakib714@users.noreply.github.com>
2021-08-19 16:50:23 +0000

    Added Decoration!

--- main.py
+++ main.py
@@ -1,16 +1,20 @@
 import random
 
 
-print('Welcome to password generator:')
+print('Welcome to password generator:') # WElcome Note
 
+
+# Random Character
 charecters = 'qweryuioplkjhdsaazxcvbnmQWERTYUIOPLKJHGFAAZXXCCVBNM123456789@#$%&*'
 
+
+# User Input
 how_many = int(input('How many password do you want to generate: '))
 length = int(input("Enter the length of your password: "))
 
-
+# Password Generation Process
 for i in range(how_many):
-    
+
     password = ''
 
     for i in range(length):
